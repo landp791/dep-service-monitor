c343a30d21d5a3c4075ae644e895b23dafb84658
landp791 <landongpingpub@163.com>
2014-06-15 23:13:37 +0800

    tmp version

--- src/main/java/com/dep/monitor/controller/MonitorController.java
+++ src/main/java/com/dep/monitor/controller/MonitorController.java
@@ -1,12 +1,17 @@
 package com.dep.monitor.controller;
 
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.dep.monitor.model.ApplicationOwner;
+import com.dep.monitor.repo.AppOwnerRepository;
 import com.dep.monitor.service.MonitorService;
 
 @Controller
@@ -16,11 +21,19 @@ public class MonitorController {
 	@Autowired
 	private MonitorService monitorService;
 	
-	
+	@Autowired
+	private AppOwnerRepository repository;
 
 	@RequestMapping(value="/app/monitor")
-	public void monitorSpecifiedService(@RequestParam("serviceUrl")String serviceUrl) {
+	public void monitorSpecifiedService(@RequestParam("serviceUrl")String url) {
+		List<ApplicationOwner> appOwners = repository.queryByUrl(url);
+		if (CollectionUtils.isEmpty(appOwners)) {
+			logger.warn("No config for this url!["+ url +"]");
+			return;
+		}
+		int respCode = monitorService.tryToMonitor(url);
 		
+		monitorService.sendEmails(url, appOwners, respCode);
 	}
 	
 	@RequestMapping(value="/all/monitor")
--- src/main/java/com/dep/monitor/service/MonitorService.java
+++ src/main/java/com/dep/monitor/service/MonitorService.java
@@ -1,8 +1,27 @@
 package com.dep.monitor.service;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.springframework.stereotype.Service;
+
+import com.dep.monitor.model.ApplicationOwner;
 
 @Service
 public class MonitorService {
+
+	public int tryToMonitor(String url) {
+		return 0;
+	}
+
+	public void sendEmails(String url, List<ApplicationOwner> appOwners, int respCode) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }

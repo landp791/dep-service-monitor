83a5e93d110f0ee8124c013147ed6a3e31082bb5
landp791 <landongpingpub@163.com>
2014-06-23 23:58:01 +0800

    modify

--- src/main/webapp/WEB-INF/db/creatable.sql
+++ src/main/webapp/WEB-INF/db/creatable.sql
@@ -1,5 +1,5 @@
-create table service(serviceId varchar(42), serviceName varchar(100), serviceUrl varchar(200), constraint pk_service primary key (serviceId));
+create table app(serviceId varchar(42), serviceName varchar(100), serviceUrl varchar(200), constraint pk_service primary key (serviceId));
 
 create table owner(umAccount varchar(50), name varchar(20), mail varchar(60), constraint pk_owner primary key (umAccount));
        
-create table serviceOwner(serviceId varchar(42), umAccount varchar(50), constraint pk_service_owner primary key (serviceId, umAccount)); 
+create table appOwner(serviceId varchar(42), umAccount varchar(50), constraint pk_service_owner primary key (serviceId, umAccount)); 

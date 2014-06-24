79ab9f3e25957dd1d7692b1e682bad0773a5e04b
landongping791 <landongping791@pingan.com>
2014-06-24 18:14:13 +0800

    add app owner oper

---
+++ src/main/java/com/dep/monitor/controller/AppAdminController.java
@@ -0,0 +1,13 @@
+package com.dep.monitor.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AppAdminController {
+	private static final Log logger = LogFactory.getLog(MonitorController.class);
+	
+	
+	
+}
